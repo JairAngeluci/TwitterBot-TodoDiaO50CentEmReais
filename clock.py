394bd372a2605c0f27cc260f807fd55d1fd96d8d
Jair Angeluci <34008533+JairAngeluci@users.noreply.github.com>
2022-02-28 13:50:57 -0300

    postar menos
    ta chatao, vou deixar postando so uma vez no dia, deixei so um schedule pra seg ate sex pra 12h

--- clock.py
+++ clock.py
@@ -2,14 +2,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler(timezone="america/sao_paulo")
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=12)
 def scheduled_job():
     print('Exec 1000...')
-    exec(open('TodoDiaO50CentEmReais.py').read())
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=18)
-def scheduled_job():
-    print('Exec 1800...')
     exec(open('TodoDiaO50CentEmReais.py').read())
 
 sched.start()
